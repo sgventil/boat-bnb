01574f1b2768ca5c5ca93db871ef7698d6ee4d3e
nicola camp <nicola.camp89@yahoo.com>
2023-08-24 09:03:03 +0100

    booking form added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @boat = Boat.find(params[:boat_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @booking.boat = @boat
+    @booking.user = current_user
+    @booking.save
+    redirect_to root_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:content, :booking_date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :boats
+  resources :boats do
+    resources :bookings
+  end
 end
