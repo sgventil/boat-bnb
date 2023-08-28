8bc7a66241b71bb8db768ffdcd36dfeb5756cf26
Mardgelle Neri <mardgelleneri@gmail.com>
2023-08-28 16:25:49 +0100

    new and create actions working however redirecting to boats index - will correct this to redirect to boats show page. Review index is working and showing on the boats show page. Authorisations are working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,40 @@
 class ReviewsController < ApplicationController
-  def new
+  before_action :set_review, only: %i[ show destroy confirmation ]
+  def index
+    @bookings = policy_scope(Review).where(boat: boat)
   end
+
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+    authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @review.user = current_user
+    @review.boat = @review.booking.boat
+
+    authorize @review
+
+    if @review.save
+      redirect_to boats_path(@review.boat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
+
 end
