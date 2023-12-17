77c1a59037381eea4809bddddf3d550684cf1024
Fernando Ribeiro <pontogponto@gmail.com>
2023-12-17 19:49:32 +0100

    rewrite the button controller to handle screen size or orientation changes

--- app/javascript/controllers/btn_controller.js
+++ app/javascript/controllers/btn_controller.js
@@ -238,6 +238,66 @@
 // };
 
 
+// import { Controller } from "@hotwired/stimulus";
+
+// export default class extends Controller {
+//   connect() {
+//     const btn = document.getElementById('btn');
+//     const cardGrid = document.getElementById('gridjs');
+//     const mapContainer = document.getElementById('mapjs');
+
+//     const isMobilePortrait = () => {
+//       return window.innerWidth <= 575.98 && window.innerHeight > window.innerWidth;
+//     };
+
+//     const showCardGrid = () => {
+//       cardGrid.style.display = 'grid';
+//       mapContainer.style.display = 'none';
+//       btn.textContent = 'Map';
+//     };
+
+//     const showMap = () => {
+//       cardGrid.style.display = 'none';
+//       mapContainer.style.display = 'block';
+//       btn.textContent = 'Boats';
+//     };
+
+//     btn.addEventListener('click', (event) => {
+//       event.preventDefault();
+//       if (isMobilePortrait()) {
+//         if (window.getComputedStyle(cardGrid).display !== 'none') {
+//           showMap();
+//         } else {
+//           showCardGrid();
+//         }
+//       }
+//     });
+
+//     const handleResize = () => {
+//       const isPortrait = isMobilePortrait();
+//       if (isPortrait) {
+//         showCardGrid();
+//       } else {
+//         cardGrid.style.display = 'grid';
+//         mapContainer.style.display = 'block';
+//         btn.textContent = 'Boats';
+//       }
+//     };
+
+//     window.addEventListener('resize', handleResize);
+//     window.addEventListener('orientationchange', handleResize);
+
+//     // Set the initial view based on the screen size
+//     if (isMobilePortrait()) {
+//       showCardGrid();
+//     } else {
+//       cardGrid.style.display = 'grid';
+//       mapContainer.style.display = 'block';
+//       btn.textContent = 'Boats';
+//     }
+//   }
+// };
+
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
@@ -245,6 +305,7 @@ export default class extends Controller {
     const btn = document.getElementById('btn');
     const cardGrid = document.getElementById('gridjs');
     const mapContainer = document.getElementById('mapjs');
+    let isMapTouched = false; // Variable to track touch events in map
 
     const isMobilePortrait = () => {
       return window.innerWidth <= 575.98 && window.innerHeight > window.innerWidth;
@@ -286,6 +347,20 @@ export default class extends Controller {
 
     window.addEventListener('resize', handleResize);
     window.addEventListener('orientationchange', handleResize);
+
+    mapContainer.addEventListener('touchstart', () => {
+      isMapTouched = true;
+    });
+
+    mapContainer.addEventListener('touchmove', (event) => {
+      if (isMapTouched) {
+        event.preventDefault(); // Prevent default touchmove behavior if in map view
+      }
+    });
+
+    mapContainer.addEventListener('touchend', () => {
+      isMapTouched = false;
+    });
 
     // Set the initial view based on the screen size
     if (isMobilePortrait()) {
